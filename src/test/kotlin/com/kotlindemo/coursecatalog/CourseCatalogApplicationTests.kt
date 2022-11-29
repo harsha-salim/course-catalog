02d528ef6db91859f83eacb9f2037b6fb9a0d77c
Harsha Salim <harsha.salim@thoughtworks.com>
2022-11-29 17:04:37 +0530

    Project setup

---
+++ src/test/kotlin/com/kotlindemo/coursecatalog/CourseCatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlindemo.coursecatalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
