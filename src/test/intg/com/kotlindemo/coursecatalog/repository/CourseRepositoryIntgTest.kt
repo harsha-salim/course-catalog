0ec6d530d92e8880dac532dabbc0ded04ff3176d
Harsha Salim <harsha.salim@thoughtworks.com>
2023-03-25 11:32:23 +0530

    Add. Test Container to Repository Integration Tests

--- src/test/intg/com/kotlindemo/coursecatalog/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlindemo/coursecatalog/repository/CourseRepositoryIntgTest.kt
@@ -9,18 +9,49 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
+@Testcontainers
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class CourseRepositoryIntgTest {
     @Autowired
     lateinit var courseRepository: CourseRepository
 
     @Autowired
     lateinit var instructorRepository: InstructorRepository
+    companion object {
+
+        @Container
+        val postgresDB = PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:13-alpine")).apply {
+            withDatabaseName("testdb")
+            withUsername("postgres")
+            withPassword("secret")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresDB::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresDB::getUsername)
+            registry.add("spring.datasource.password", postgresDB::getPassword)
+        }
+
+        @JvmStatic
+        fun courseNameAndSize():Stream<Arguments>{
+            return Stream.of(Arguments.arguments("Plants",2), Arguments.arguments("Animals",1))
+        }
+    }
 
     @BeforeEach
     fun setup(){
@@ -57,10 +88,4 @@ class CourseRepositoryIntgTest {
         Assertions.assertEquals(size,courses.size)
     }
 
-    companion object{
-        @JvmStatic
-        fun courseNameAndSize():Stream<Arguments>{
-            return Stream.of(Arguments.arguments("Plants",2), Arguments.arguments("Animals",1))
-        }
-    }
 }
